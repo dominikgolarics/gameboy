ba8b4bd7585ed7978643526c31a1118cf9e857a7
dominikgolarics <72364663146@szily.hu>
2025-02-25 13:10:51 +0100

    games.php tesztre kész

--- games.php
+++ games.php
@@ -13,17 +13,25 @@ class Games
         $dbvez->closeDB();
     }
 
-    public function getAllGames()
+    public function GetAllGames()
     {
         return $this->games;
     }
 
-    public function getGamesById($GameId)
+    public function GetGamesById($gameId)
     {
-       $query = "SELECT g_ID, name, pic FROM `games` WHERE g_ID=".$GameId;
+       $query = "SELECT g_ID, name, pic FROM `games` WHERE g_ID=".$gameId;
        $dbvez = new DBVezerelo();
        $this->games=$dbvez->executeSelectQuery($query);
        $dbvez->closeDB();
        return $this->games;
+    }
+
+    public function GetGamesByType($typeName){
+        $query = "SELECT g_ID, name, pic FROM `games` INNER JOIN `type` ON games.type_ID=type.t_ID WHERE type.name='".$typeName."'";
+        $dbvez = new DBVezerelo();
+        $this->games=$dbvez->executeSelectQuery($query);
+        $dbvez->closeDB();
+        return $this->games;
     }
 }
